7d69b0f93a4b3990ca5c744bf292bb5c2a1f16fc
Tafarel Yan <10773291+tafarelyan@users.noreply.github.com>
2024-12-09 10:34:22 -0300

    Update convert_excel_file_url_to_list.py

--- convert_excel_file_url_to_list.py
+++ convert_excel_file_url_to_list.py
@@ -1,9 +1,6 @@
 import pandas as pd
 
-def main(input):
-    excel_file_url = input['excel_file_url']
-    sheet_name = input.get('sheet_name', 0)
-    
+def convert_excel_file_url_to_list_of_dicts(excel_file_url, sheet_name=0):
     df = pd.read_excel(excel_file_url, sheet_name=sheet_name, dtype=str)
     df.fillna('', inplace=True)
     
